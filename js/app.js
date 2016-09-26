4bf0adc499ba7391cd0c9da8665f092b32c93fa7
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-26 07:13:50 -0800

    agregando intentacion

--- js/app.js
+++ js/app.js
@@ -9,59 +9,47 @@
         var dia = document.getElementsByClassName("alimento");
         var seccion = document.getElementById("menu-de-frutas");
         var contador = 0;
-
         for (var i = 0; i < dia.length; i++) {
             manzana.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
-
             dia[i].addEventListener("drop", function(evento) {
                 clonar(evento, this);
-
             });
-
             dia[i].addEventListener("dragover", function(ev) {
                 ev.preventDefault();
             });
-
             platano.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
-
             naranja.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
-
             mandarina.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
-
             fresa.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
         }
-
-       
         function inicio(e) {
-            e.dataTransfer.setData("img", e.target.id); //Coje el elemento que se va a mover
+            e.dataTransfer.setData("img", e.target.id); 
         }
 
         function clonar(e) {
-            var elementoArrastrado = document.getElementById(e.dataTransfer.getData("img")); // Elemento arrastrado
-            elementoArrastrado.style.opacity = ''; // Dejamos la opacidad a su estado anterior para copiar el elemento igual que era antes
-            var elementoClonado = elementoArrastrado.cloneNode(true); // Se clona el elemento
-            elementoClonado.id = "ElemClonado" + contador; // Se cambia el id porque tiene que ser unico
+            var elementoArrastrado = document.getElementById(e.dataTransfer.getData("img")); 
+            elementoArrastrado.style.opacity = ''; 
+            var elementoClonado = elementoArrastrado.cloneNode(true); 
+            elementoClonado.id = "ElemClonado" + contador;
             contador += 1;
-            elementoClonado.style.position = "static"; // Se posiciona de forma "normal" (Sino habria que cambiar las coordenadas de la posición)
-            e.target.appendChild(elementoClonado); // Se añade el elemento clonado
-            e.target.style.border = ''; // Quita el borde del "cuadro clonador"
+            elementoClonado.style.position = "static"; 
+            e.target.appendChild(elementoClonado); 
+            e.target.style.border = ''; 
             	elementoClonado.addEventListener("dblclick",function(){
             		elementoClonado.style.display = "none";
             	});
         }
-
     });
-
 })()
 
     
